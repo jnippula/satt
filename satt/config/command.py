07e3378f05616d2b9dc0183098307a58bd59bf2e
Jari Nippula <jari.nippula@intel.com>
2020-03-23 13:03:30 +0200

    2to3: satt/config/command.py

--- satt/config/command.py
+++ satt/config/command.py
@@ -50,41 +50,41 @@ class SattConfig:
     # ===============================================#
     def action(self):
         os.system('cls' if os.name == 'nt' else 'clear')
-        print '**************************************'
-        print '*  Setup SATT environment variables! *'
-        print '*  (*) [Enter]-key to choose default *'
-        print '**************************************\n'
-        print helper.color.BOLD + 'Current configuration:' + helper.color.END
+        print('**************************************')
+        print('*  Setup SATT environment variables! *')
+        print('*  (*) [Enter]-key to choose default *')
+        print('**************************************\n')
+        print(helper.color.BOLD + 'Current configuration:' + helper.color.END)
         configurations_exits = True
         if self._variables['sat_os'] >= 0:
-            print ('   ' + helper.color.BOLD + targetos.OsHelper.osdata[self._variables['sat_os']].Name + helper.color.END)
-            print ('   ' + '-' * len(targetos.OsHelper.osdata[self._variables['sat_os']].Name))
-            print ('   Trace out     : ' + helper.color.BOLD + self._variables['sat_trace_logging_method'] + helper.color.END)
-            print ('   Control bus   : ' + helper.color.BOLD + self._variables['sat_control_bus'] + helper.color.END),
+            print('   ' + helper.color.BOLD + targetos.OsHelper.osdata[self._variables['sat_os']].Name + helper.color.END)
+            print('   ' + '-' * len(targetos.OsHelper.osdata[self._variables['sat_os']].Name))
+            print('   Trace out     : ' + helper.color.BOLD + self._variables['sat_trace_logging_method'] + helper.color.END)
+            print(('   Control bus   : ' + helper.color.BOLD + self._variables['sat_control_bus'] + helper.color.END), end=' ')
             if self._variables['sat_control_bus'] == 'SSH':
-                print '-> ' + helper.color.BOLD + self._variables['sat_control_ip'] + helper.color.END
+                print('-> ' + helper.color.BOLD + self._variables['sat_control_ip'] + helper.color.END)
             else:
-                print
+                print()
             if self._variables['sat_target_source'] != "":
-                print ('   Build path    : ' + helper.color.BOLD + self._variables['sat_target_source'] + helper.color.END)
+                print('   Build path    : ' + helper.color.BOLD + self._variables['sat_target_source'] + helper.color.END)
             if os.path.basename(self._variables['sat_target_build']) != "":
-                print ('   Product       : ' + helper.color.BOLD + os.path.basename(self._variables['sat_target_build']) +
+                print('   Product       : ' + helper.color.BOLD + os.path.basename(self._variables['sat_target_build']) +
                        helper.color.END)
             if self._variables['sat_path_kernel'] != "":
-                print ('   Kernel path   : ' + helper.color.BOLD + self._variables['sat_path_kernel'] + helper.color.END)
+                print('   Kernel path   : ' + helper.color.BOLD + self._variables['sat_path_kernel'] + helper.color.END)
 
-            print
-            print helper.color.BOLD + 'Change values:' + helper.color.END
-            print ' * 0) No  - keep current setup'
-            print '   1) Yes - select setup'
+            print()
+            print(helper.color.BOLD + 'Change values:' + helper.color.END)
+            print(' * 0) No  - keep current setup')
+            print('   1) Yes - select setup')
             selection = self._readchar()
 
         else:
-            print "   None"
+            print("   None")
             selection = '1'
             configurations_exits = False
 
-        print
+        print()
 
         if selection == '1':
             if configurations_exits:
@@ -98,28 +98,28 @@ class SattConfig:
             self._config.add_config(self._variables)
     # ===============================================#
     def set_os(self):
-        print helper.color.BOLD + 'Select target OS:' + helper.color.END
-        for osid in targetos.OsHelper.osdata.keys():
+        print(helper.color.BOLD + 'Select target OS:' + helper.color.END)
+        for osid in list(targetos.OsHelper.osdata.keys()):
             if osid == self._variables['sat_os']:
-                print ' *',
+                print(' *', end=' ')
             else:
-                print '  ',
-            print str(osid) + ') ' + targetos.OsHelper.osdata[osid].Name
+                print('  ', end=' ')
+            print(str(osid) + ') ' + targetos.OsHelper.osdata[osid].Name)
         selection = self._readchar()
         # default: set first item
         if selection.isdigit():
-            if int(selection) in targetos.OsHelper.osdata.keys():
+            if int(selection) in list(targetos.OsHelper.osdata.keys()):
                 value = int(selection)
                 self._variables['sat_os'] = value
                 self._config.set_variable('sat_os', value)
 
-        print ('   Selected OS = ' + helper.color.BOLD + targetos.OsHelper.osdata[self._variables['sat_os']].Name +
+        print('   Selected OS = ' + helper.color.BOLD + targetos.OsHelper.osdata[self._variables['sat_os']].Name +
                helper.color.END + '\n')
 
     # ===============================================#
     def set_trace_destination(self):
         self._variables['sat_trace_logging_method'] = 'RAM'
-        print ('Autoselect trace destination: ' + helper.color.BOLD +
+        print('Autoselect trace destination: ' + helper.color.BOLD +
                self._variables['sat_trace_logging_method'] + helper.color.END + '\n')
         # methods = ['RAM', 'USB3']
         # h = helper.get_instance()
@@ -148,19 +148,19 @@ class SattConfig:
         # methods = ['ADB', 'SSH', 'SHELL']
         methods = targetos.OsHelper.osdata[self._variables['sat_os']].ConnMethods
         if len(methods) == 1:
-            print 'Autoselect control bus: ' + helper.color.BOLD + methods[0] + helper.color.END + '\n'
+            print('Autoselect control bus: ' + helper.color.BOLD + methods[0] + helper.color.END + '\n')
             self._variables['sat_control_bus'] = methods[0]
             self._config.set_variable('sat_control_bus', methods[0])
         else:
-            print helper.color.BOLD + 'Select control bus:' + helper.color.END
+            print(helper.color.BOLD + 'Select control bus:' + helper.color.END)
             index = 0
             for idx, val in enumerate(methods):
                 if val == self._variables['sat_control_bus']:
                     index = idx
-                    print ' *',
+                    print(' *', end=' ')
                 else:
-                    print '  ',
-                print str(idx) + ') ' + val
+                    print('  ', end=' ')
+                print(str(idx) + ') ' + val)
             while True:
                 value = self._readchar()
                 if value.isdigit():
@@ -171,7 +171,7 @@ class SattConfig:
                     break
             self._variables['sat_control_bus'] = methods[index]
             self._config.set_variable('sat_control_bus', methods[index])
-            print '   Control bus = ' + self._variables['sat_control_bus'] + '\n'
+            print('   Control bus = ' + self._variables['sat_control_bus'] + '\n')
 
         if self._variables['sat_control_bus'] == 'SSH':
             self.set_control_ip()
@@ -180,12 +180,12 @@ class SattConfig:
             self._variables['sat_control_ip'] = ''
 
     def set_login_id(self):
-        print helper.color.BOLD + 'Set Login ID for the SSH:' + helper.color.END
+        print(helper.color.BOLD + 'Set Login ID for the SSH:' + helper.color.END)
         if self._variables['sat_login_id'] == '':
             _temp_login_id = 'No Login ID'
         else:
             _temp_login_id = '*current: %s' %(self._variables['sat_login_id'],)
-        _login_id = raw_input('   Give Login ID (%s): ' %(_temp_login_id, ))
+        _login_id = input('   Give Login ID (%s): ' %(_temp_login_id, ))
         if _login_id != '':
             self._variables['sat_login_id'] = _login_id
         print('   Using Login ID:%s\n' %(self._variables['sat_login_id'],))
@@ -193,13 +193,13 @@ class SattConfig:
     # ===============================================#
     def set_control_ip(self):
         # '--ip', action='store', help='IP Address to connect, e.g. --ip 192.168.1.10'
-        print helper.color.BOLD + 'Set SSH IP address:' + helper.color.END
+        print(helper.color.BOLD + 'Set SSH IP address:' + helper.color.END)
         correct = False
         while correct is False:
             correct = True
             if self._variables['sat_control_ip'] == '':
                 self._variables['sat_control_ip'] = '127.0.0.1'
-            ip = raw_input('   Give IP address to connect (current %s): ' %(self._variables['sat_control_ip'], ))
+            ip = input('   Give IP address to connect (current %s): ' %(self._variables['sat_control_ip'], ))
             if ip == '':
                 break
             values = ip.split('.')
@@ -212,43 +212,43 @@ class SattConfig:
                 correct = False
             if correct is True:
                 self._variables['sat_control_ip'] = ip
-                print '   IP address = ' + ip
+                print('   IP address = ' + ip)
             else:
-                print "Not a valid IP address format, try again"
-        print
+                print("Not a valid IP address format, try again")
+        print()
 
     # ===============================================#
     def select_prev_setup(self):
         setups = self._config.get_config_list()
-        print helper.color.BOLD + 'Previous setups:' + helper.color.END
+        print(helper.color.BOLD + 'Previous setups:' + helper.color.END)
         max_key = max(len(targetos.OsHelper.osdata[x].Name) for x in targetos.OsHelper.osdata)
         last_setup = len(setups) - 1
         for idx, setup in enumerate(setups):
             if idx > 0:
                 if idx == 1:
-                    print '   ' + str(idx) + ') New setup'
+                    print('   ' + str(idx) + ') New setup')
                 else:
                     star = ' '
                     if last_setup == idx:
                         star = '*'
-                    print (' ' + star + ' ' + str(idx) + ') ' + helper.color.BOLD +
-                           targetos.OsHelper.osdata[setup['sat_os']].Name.ljust(max_key) + helper.color.END),
-                    print (': ' + helper.color.BOLD + setup['sat_trace_logging_method'] + helper.color.END +
-                           '/' + helper.color.BOLD + setup['sat_control_bus'] + helper.color.END),
+                    print((' ' + star + ' ' + str(idx) + ') ' + helper.color.BOLD +
+                           targetos.OsHelper.osdata[setup['sat_os']].Name.ljust(max_key) + helper.color.END), end=' ')
+                    print((': ' + helper.color.BOLD + setup['sat_trace_logging_method'] + helper.color.END +
+                           '/' + helper.color.BOLD + setup['sat_control_bus'] + helper.color.END), end=' ')
                     if setup['sat_control_ip'] != '':
-                        print '-> ' + helper.color.BOLD + setup['sat_control_ip'] + helper.color.END
+                        print('-> ' + helper.color.BOLD + setup['sat_control_ip'] + helper.color.END)
                     else:
-                        print
+                        print()
                     if setup['sat_target_source'] != "":
-                        print ('        path: ' + helper.color.BOLD + setup['sat_target_source'] + helper.color.END)
+                        print('        path: ' + helper.color.BOLD + setup['sat_target_source'] + helper.color.END)
                     if os.path.basename(setup['sat_target_build']) != "":
-                        print ('        prod: ' + helper.color.BOLD + os.path.basename(setup['sat_target_build']) +
+                        print('        prod: ' + helper.color.BOLD + os.path.basename(setup['sat_target_build']) +
                                helper.color.END)
                     if self._variables['sat_path_kernel'] != "":
-                        print ('        kern: ' + helper.color.BOLD + setup['sat_path_kernel'] + helper.color.END)
+                        print('        kern: ' + helper.color.BOLD + setup['sat_path_kernel'] + helper.color.END)
 
         while True:
-            value = raw_input('   :')
+            value = input('   :')
             if value.isdigit():
                 if int(value) >= 0 and int(value) <= len(setups):
                     self._variables = self._config.get_config(int(value))
@@ -259,7 +259,7 @@ class SattConfig:
     # ===============================================#
     def debug_print(self, idx):
         l = self._config.get_config_list()
-        print ">>>" + str(idx)
+        print(">>>" + str(idx))
         for i, setup in enumerate(l):
-            print str(i) + ') ' + setup['sat_target_source']
-        print "<<<" + str(idx)
+            print(str(i) + ') ' + setup['sat_target_source'])
+        print("<<<" + str(idx))
